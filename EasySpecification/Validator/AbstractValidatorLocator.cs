54321c53e65175c4e3086387648f69967e1739d6
André Davis <cubicle.jockey@live.com>
2017-12-05 11:40:13 -0800

    Created Abstract Validator Locator

---
+++ EasySpecification/Validator/AbstractValidatorLocator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EasySpecification.Validator
+{
+    public abstract class AbstractValidatorLocator
+    {
+        protected readonly IServiceProvider ServiceProvider;
+
+        protected AbstractValidatorLocator(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        public virtual IValidatorAsync<TEntity> GetValidator<TEntity>() where TEntity : class
+        {
+            return (IValidatorAsync<TEntity>)ServiceProvider.GetService(typeof(IValidatorAsync<TEntity>));
+        }
+    }
+}
