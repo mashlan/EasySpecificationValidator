39b509680d2b7e41b867bd48ddfb6aad05bdfb58
André Davis <cubicle.jockey@live.com>
2017-12-06 09:33:24 -0800

    Created a base class for testing Specifications. Created AndSpecification tests.

---
+++ EasySpecificationValidator.Tests/Specification/BaseTest.cs
@@ -0,0 +1,43 @@
+﻿using EasySpecificationValidator.Specification;
+using FakeItEasy;
+using System;
+using System.Linq.Expressions;
+
+namespace EasySpcificationValidator.Tests.Specification
+{
+    public class BaseTest<TSpecificationEntity>
+    {
+        protected readonly ISpecification<TSpecificationEntity> LeftSpecification;
+        protected readonly ISpecification<TSpecificationEntity> RightSpecification;
+
+        protected readonly Expression<Func<bool>> LeftSpecExpressionIsSatisfiedBy;
+        protected readonly Expression<Func<bool>> RightSpecExpressionIsSatisfiedBy;
+
+        public BaseTest()
+        {
+            LeftSpecification = A.Fake<ISpecification<TSpecificationEntity>>();
+            RightSpecification = A.Fake<ISpecification<TSpecificationEntity>>();
+
+            LeftSpecExpressionIsSatisfiedBy = () => LeftSpecification.IsSatisfiedBy(A<TSpecificationEntity>.Ignored);
+            RightSpecExpressionIsSatisfiedBy = () => RightSpecification.IsSatisfiedBy(A<TSpecificationEntity>.Ignored);
+        }
+
+        protected void SetLeftAndRightExpressionsResults(bool leftResult, bool rightResult)
+        {
+            A.CallTo(LeftSpecExpressionIsSatisfiedBy).Returns(leftResult);
+            A.CallTo(RightSpecExpressionIsSatisfiedBy).Returns(rightResult);
+        }
+
+        protected void CheckCallTosOfLeftAndRightExpressions(Repeated leftCallCount, Repeated rightCallCount)
+        {
+            A.CallTo(LeftSpecExpressionIsSatisfiedBy).MustHaveHappened(leftCallCount);
+            A.CallTo(RightSpecExpressionIsSatisfiedBy).MustHaveHappened(rightCallCount);
+        }
+
+        protected void ClearFakes()
+        {
+            Fake.ClearConfiguration(LeftSpecification);
+            Fake.ClearConfiguration(RightSpecification);
+        }
+    }
+}
