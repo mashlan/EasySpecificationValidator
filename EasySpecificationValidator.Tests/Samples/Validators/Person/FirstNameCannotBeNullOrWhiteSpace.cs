8c0e23130247a5ff89330965b336a603b95b296e
André Davis <cubicle.jockey@live.com>
2017-12-07 07:04:57 -0800

    Cleanup

--- EasySpecificationValidator.Tests/Samples/Validators/Person/FirstNameCannotBeNullOrWhiteSpace.cs
+++ EasySpecificationValidator.Tests/Samples/Validators/Person/FirstNameCannotBeNullOrWhiteSpace.cs
@@ -7,7 +7,7 @@ namespace EasySpecificationValidator.Tests.Samples.Validators.Person
     {
         #region Overrides of GenericSpecification<Person>
 
-        public override Func<Entities.Person, bool> Rule => a => !string.IsNullOrWhiteSpace(a.FirstName);
+        public override Func<Entities.Person, bool> Rule => person => !string.IsNullOrWhiteSpace(person.FirstName);
 
         #endregion
     }
